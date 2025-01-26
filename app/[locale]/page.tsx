799cbe205defa2c2c64d5fd2b99441065840f1bd
omnia <omniakhaled239@gmail.com>
2025-01-26 12:11:39 +0200

    feat: add HomeContent component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,13 @@
+"use client";
 import { MainCarousel } from "@/components/common/home/MainCarousel";
 import * as React from "react";
+import HomeContent from "@/components/common/home/HomeContent";
 
 export default function Page() {
   return (
-    <>
+    <div className="w-full pt-5 px-3">
       <MainCarousel />
-    </>
+      <HomeContent />
+    </div>
   );
 }
