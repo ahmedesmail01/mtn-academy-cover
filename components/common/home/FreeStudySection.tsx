711ab840445f6b2dae72f750b5adcd4525b4a78c
omnia <omniakhaled239@gmail.com>
2025-01-26 17:13:13 +0200

    refactor: update FreeStudySection component

--- components/common/home/FreeStudySection.tsx
+++ components/common/home/FreeStudySection.tsx
@@ -1,14 +1,13 @@
-
 import StudyCard from "./StudyCard";
 
 export default function FreeStudySection() {
   return (
-    <div className="container w-[80%] mx-auto mt-20">
-      <div className="flex items-end justify-between">
-        <h1 className="text-4xl font-bold text-[#353535]">
+    <div className="container md:w-[80%] mx-auto mt-20">
+      <div className="flex items-end justify-center md:justify-between">
+        <h1 className="text-3xl md:text-4xl font-bold text-[#353535]">
           Free Study Section
         </h1>
-        <span className="text-lg">See all</span>
+        <span className="hidden md:inline text-lg">See all</span>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 gap-5">
         {Array.from({ length: 8 }).map((_, index) => {
