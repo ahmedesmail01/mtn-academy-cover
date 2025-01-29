9107ce6d24fd711cfb524eaae6a706a7eb074e6d
Marina Safwat <66208355+marinaSwadee@users.noreply.github.com>
2025-01-29 13:26:42 +0200

    feat: implement automatic redirection to English page on load

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/en");
+  };
+
+  useEffect(() => {
+    handleClick();
+  });
+
   return <></>;
 }
